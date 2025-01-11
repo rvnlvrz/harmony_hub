a107d3c322d9085edbac7160d9133848b550212b
Arvin Kenneth C. Alvarez <rvnlvrz@gmail.com>
2025-01-11 10:27:23 +0800

    Add auth0 integration (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:harmony_hub/profile_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,27 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,25 +34,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   int _selectedIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Credentials? _credentials;
+  late Auth0 auth0;
+
+  @override
+  void initState() {
+    super.initState();
+    auth0 =
+        Auth0('dev-gsbc-org.us.auth0.com', 'H0oLVM5XrHC08ljf2sBkvvtPF9djvs80');
   }
 
   void changeSelectedTab(int value) {
     setState(() {
       _selectedIndex = value;
-      
-      if(kDebugMode){
+
+      if (kDebugMode) {
         print('Selected index: $value');
       }
     });
@@ -82,66 +58,72 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard_sharp), label: 'Dashboard'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.check_circle_outline), label: 'Attendance')
-        ],
-        onTap: changeSelectedTab,
-        currentIndex: _selectedIndex,
-      ),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard_sharp), label: 'Dashboard'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.check_circle_outline), label: 'Attendance')
+          ],
+          onTap: changeSelectedTab,
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: context.colors.scheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              ProfileView(
+                credentials: _credentials,
+                onPrimaryAction: doSomething,
+                primaryActionText: _credentials == null ? 'Log in' : 'Log out',
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _startSession,
+        tooltip: 'Start Session',
+        child: const Icon(Icons.timer_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<void> doSomething() async {
+    if (_credentials == null) {
+      return loginAsync();
+    } else {
+      return logoutAsync();
+    }
+  }
+
+  Future<void> logoutAsync() async {
+    // Use a Universal Link logout URL on iOS 17.4+ / macOS 14.4+
+    // useHTTPS is ignored on Android
+    await auth0.webAuthentication(scheme: 'harmonyhub').logout(useHTTPS: true);
+    setState(() {
+      _credentials = null;
+    });
+  }
+
+  Future<void> loginAsync() async {
+    // Use a Universal Link callback URL on iOS 17.4+ / macOS 14.4+
+    // useHTTPS is ignored on Android
+    final credentials = await auth0
+        .webAuthentication(scheme: 'harmonyhub')
+        .login(useHTTPS: true);
+
+    setState(() {
+      _credentials = credentials;
+    });
+  }
+
+  void _startSession() {}
 }
