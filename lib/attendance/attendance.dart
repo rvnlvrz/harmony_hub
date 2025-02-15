9b3bc23ff6679a998533f0133fbb74cdabf90d9d
Arvin Kenneth C. Alvarez <rvnlvrz@gmail.com>
2025-02-15 14:06:06 +0800

    Refactor filter chip into separate class

--- lib/attendance/attendance.dart
+++ lib/attendance/attendance.dart
@@ -1,9 +1,9 @@
 import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:harmony_hub/attendance/attendance_filter_chip.dart';
 import 'package:harmony_hub/session/session_view.dart';
 
-import 'attendance_state.dart';
 // import 'package:harmony_hub/shared/infrastructure/dto/people.dart';
 
 class Attendance extends ConsumerWidget {
@@ -61,73 +61,5 @@ class Attendance extends ConsumerWidget {
             AttendanceFilterChip(),
           ]),
     ));
-  }
-}
-
-class AttendanceFilterChip extends ConsumerWidget {
-  const AttendanceFilterChip({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final attendanceStateNotifier =
-        ref.read(attendanceStateNotifierProvider.notifier);
-
-    final attendanceState = ref.watch(attendanceStateNotifierProvider);
-
-    return Wrap(
-      spacing: 5.0,
-      children: [
-        FilterChip(
-          label: Text('Ready'),
-          selected: attendanceState.filters.contains(AttendanceFilter.ready),
-          onSelected: (bool selected) {
-            if (selected) {
-              attendanceStateNotifier.addFilter(AttendanceFilter.ready);
-            } else {
-              attendanceStateNotifier.removeFilter(AttendanceFilter.ready);
-            }
-          },
-        ),
-        FilterChip(
-          label: Text('Present'),
-          selected: attendanceState.filters.contains(AttendanceFilter.present),
-          onSelected: (bool selected) {
-            if (selected) {
-              attendanceStateNotifier.addFilter(AttendanceFilter.present);
-            } else {
-              attendanceStateNotifier.removeFilter(AttendanceFilter.present);
-            }
-          },
-        ),
-        FilterChip(
-          label: Text('Absent'),
-          selected: attendanceState.filters.contains(AttendanceFilter.absent) ||
-              attendanceState.filters.contains(AttendanceFilter.absentExcused),
-          onSelected: (bool selected) {
-            if (selected) {
-              attendanceStateNotifier.addFilters(
-                  {AttendanceFilter.absent, AttendanceFilter.absentExcused});
-            } else {
-              attendanceStateNotifier.removeFilters(
-                  {AttendanceFilter.absent, AttendanceFilter.absentExcused});
-            }
-          },
-        ),
-        FilterChip(
-          label: Text('Late'),
-          selected: attendanceState.filters.contains(AttendanceFilter.late) ||
-              attendanceState.filters.contains(AttendanceFilter.lateExcused),
-          onSelected: (bool selected) {
-            if (selected) {
-              attendanceStateNotifier.addFilters(
-                  {AttendanceFilter.late, AttendanceFilter.lateExcused});
-            } else {
-              attendanceStateNotifier.removeFilters(
-                  {AttendanceFilter.late, AttendanceFilter.lateExcused});
-            }
-          },
-        ),
-      ],
-    );
   }
 }
