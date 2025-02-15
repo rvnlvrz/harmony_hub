e59c895d43ed8ddab47658426d73927084d7d991
Arvin Kenneth C. Alvarez <rvnlvrz@gmail.com>
2025-02-15 14:01:35 +0800

    Add Attendance State with StateNotifier and Filter Chips

--- lib/attendance/attendance.dart
+++ lib/attendance/attendance.dart
@@ -2,6 +2,8 @@ import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:harmony_hub/session/session_view.dart';
+
+import 'attendance_state.dart';
 // import 'package:harmony_hub/shared/infrastructure/dto/people.dart';
 
 class Attendance extends ConsumerWidget {
@@ -12,6 +14,10 @@ class Attendance extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // final AsyncValue<People> members = ref.watch(membersProvider);
+    // final attendanceStateNotifier =
+    //     ref.read(attendanceStateNotifierProvider.notifier);
+    //
+    // final attendanceState = ref.watch(attendanceStateNotifierProvider);
 
     return Center(
         child: Container(
@@ -21,6 +27,7 @@ class Attendance extends ConsumerWidget {
           mainAxisSize: MainAxisSize.max,
           spacing: 8,
           children: [
+            SessionView(),
             SearchAnchor(
                 viewShape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -36,6 +43,8 @@ class Attendance extends ConsumerWidget {
                     leading: const Icon(Icons.search),
                     shape: WidgetStatePropertyAll(RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10))),
+                    elevation: WidgetStatePropertyAll(0),
+                    hintText: 'Search for attendees',
                   );
                 },
                 suggestionsBuilder:
@@ -48,19 +57,77 @@ class Attendance extends ConsumerWidget {
                     );
                   });
                 }),
-            SessionView(),
             // create choice chip between ready, present, absent, and late
-            Wrap(
-              spacing: 5.0,
-              children: List<Widget>.generate(3, (int index) {
-                return FilterChip(
-                  label: Text('Item $index'),
-                  selected: false,
-                  onSelected: (bool selected) {},
-                );
-              }).toList(),
-            ),
+            AttendanceFilterChip(),
           ]),
     ));
+  }
+}
+
+class AttendanceFilterChip extends ConsumerWidget {
+  const AttendanceFilterChip({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final attendanceStateNotifier =
+        ref.read(attendanceStateNotifierProvider.notifier);
+
+    final attendanceState = ref.watch(attendanceStateNotifierProvider);
+
+    return Wrap(
+      spacing: 5.0,
+      children: [
+        FilterChip(
+          label: Text('Ready'),
+          selected: attendanceState.filters.contains(AttendanceFilter.ready),
+          onSelected: (bool selected) {
+            if (selected) {
+              attendanceStateNotifier.addFilter(AttendanceFilter.ready);
+            } else {
+              attendanceStateNotifier.removeFilter(AttendanceFilter.ready);
+            }
+          },
+        ),
+        FilterChip(
+          label: Text('Present'),
+          selected: attendanceState.filters.contains(AttendanceFilter.present),
+          onSelected: (bool selected) {
+            if (selected) {
+              attendanceStateNotifier.addFilter(AttendanceFilter.present);
+            } else {
+              attendanceStateNotifier.removeFilter(AttendanceFilter.present);
+            }
+          },
+        ),
+        FilterChip(
+          label: Text('Absent'),
+          selected: attendanceState.filters.contains(AttendanceFilter.absent) ||
+              attendanceState.filters.contains(AttendanceFilter.absentExcused),
+          onSelected: (bool selected) {
+            if (selected) {
+              attendanceStateNotifier.addFilters(
+                  {AttendanceFilter.absent, AttendanceFilter.absentExcused});
+            } else {
+              attendanceStateNotifier.removeFilters(
+                  {AttendanceFilter.absent, AttendanceFilter.absentExcused});
+            }
+          },
+        ),
+        FilterChip(
+          label: Text('Late'),
+          selected: attendanceState.filters.contains(AttendanceFilter.late) ||
+              attendanceState.filters.contains(AttendanceFilter.lateExcused),
+          onSelected: (bool selected) {
+            if (selected) {
+              attendanceStateNotifier.addFilters(
+                  {AttendanceFilter.late, AttendanceFilter.lateExcused});
+            } else {
+              attendanceStateNotifier.removeFilters(
+                  {AttendanceFilter.late, AttendanceFilter.lateExcused});
+            }
+          },
+        ),
+      ],
+    );
   }
 }
--- lib/attendance/attendance_state.dart
+++ lib/attendance/attendance_state.dart
@@ -20,4 +20,31 @@ class AttendanceState with _$AttendanceState {
 
 class AttendanceStateNotifier extends StateNotifier<AttendanceState> {
   AttendanceStateNotifier() : super(AttendanceState(filters: {}));
+
+  void addFilter(AttendanceFilter filter) {
+    state = state.copyWith(filters: {...state.filters, filter});
+  }
+
+  void addFilters(Set<AttendanceFilter> filters) {
+    state = state.copyWith(filters: {...state.filters, ...filters});
+  }
+
+  void removeFilter(AttendanceFilter filter) {
+    var filters = Set.from(state.filters);
+    filters.remove(filter);
+    state = state.copyWith(filters: {...filters});
+  }
+
+  void removeFilters(Set<AttendanceFilter> filters) {
+    Set<AttendanceFilter> updatedFilters = Set.from(state.filters);
+    for (var filter in filters) {
+      updatedFilters.remove(filter);
+    }
+    state = state.copyWith(filters: updatedFilters);
+  }
 }
+
+final attendanceStateNotifierProvider =
+    StateNotifierProvider<AttendanceStateNotifier, AttendanceState>((ref) {
+  return AttendanceStateNotifier();
+});
